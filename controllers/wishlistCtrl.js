052bcae6f3b2ebebdb0f270233807fc4ab06d672
syam. <syamnandhu3@gmail.com.>
2024-05-13 21:28:55 +0530

    wishlist success message all done

--- controllers/wishlistCtrl.js
+++ controllers/wishlistCtrl.js
@@ -5,63 +5,47 @@ const Wishlist = require('../models/wishlistModel');
 //<------------ wishlist page -------------->
 const getwishlist = async (req, res) => {
     try {
-        const userId = req.session.user;
-        const user = await User.findById(userId);
+        const userId = req.session.user , user = await User.findById(userId);
         const wishlist = await Wishlist.find({ userId: userId });
         const productIds = wishlist.map(item => item.productId);
         const products = await Product.find({ _id: { $in: productIds } });
-
-        res.render('wishlist', {
-            products : products,
-            user : user
-        });
-    } catch (error) {
-        console.log(error.message);
-        res.render('error'); 
+        res.render('wishlist', { products : products, user : user});
+    }catch (err) {
+        res.render('error',{ message : err.message}); 
     }
 };
 
 
-//<------------ adding products to wishlist -------------->
+//<------------ adding product to wishlist -------------->
 const addToWishlist = async (req, res) => {
     try {
-        console.log('The wihslist body : ',req.body)
-        const productId = req.body.productId;
-        const userId = req.session.user;
-        const existingItem = await Wishlist.findOne({
-            productId: productId,
-            userId: userId
-        });
+        const { productId ,userId } = req.body;
+        const existingItem = await Wishlist.findOne({ productId: productId, userId: userId});
         if (!existingItem) {
             if(req.body.quantity > 3){
-                console.error('Cannot add more than 3 quantity');
                 res.status(400).json({ error: 'Cannot add more than 3 quantity' });
             } else {
-                const wishlist = new Wishlist({
-                    userId,
-                    productId: req.body.productId
-                });
+                const wishlist = new Wishlist({ userId, productId: req.body.productId});
                 await wishlist.save();
                 res.status(200).json({ message: 'Product added to wishlist successfully' });
             }
         }
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ error: 'Internal server error' });
+    } catch (err) {
+        res.render('error',{ message : err.message}); 
     } };
 
 
+//<--------------------- removing product --------------------->
     const removeWishlist = async (req, res) => {
         try {
-            const userId = req.session.user;
-            const productId = req.body.productId;
-            
-            const wishlist = await Wishlist.findOneAndDelete({ userId: userId, productId: productId });
-            res.redirect('/wishlist')
+            const {userId , productId} = req.session;
+            await Wishlist.findOneAndDelete({ userId: userId, productId: productId });
+            res.redirect('/userAddress?message=New Address added&type=success')
         } catch (err) {
             res.render('error',{ message : err.message}); 
         }
     };
+
 
 module.exports = {
     getwishlist,
