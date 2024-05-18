a8c65b0a5c30624c917fa2f6291cba25dcb12a4d
syam. <syamnandhu3@gmail.com.>
2024-05-18 17:52:53 +0530

    pakages pushing

--- DB/db.js
+++ DB/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
-const uri = 'mongodb://127.0.0.1:27017/OneCart';
+const uri = 'mongodb+srv://syamnandhu3:WF71wWF0bCF4JI2R@cluster0.doa7cbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
         await mongoose.connect(uri);
 
     } catch (error) {
