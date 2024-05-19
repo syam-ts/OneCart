d9df79b37bb848898f95a7903852a54274961cdb
syam. <syamnandhu3@gmail.com.>
2024-05-19 22:35:41 +0530

    offer bug fixed

--- controllers/offerCtrl.js
+++ controllers/offerCtrl.js
@@ -5,13 +5,8 @@ const Product = require('../models/productMdl');
   const offerManagement = async (req, res) => {
     try {
         const allProducts = await Product.find({ deleted: false });
-        const productsWithOffers = await Product.find({ 'offer.0': { $exists: true } });
-
-        const finalProducts = productsWithOffers.filter(product => product.offer.length > 0);
-
-        if (finalProducts.length > 0) {
-            res.render('offerManagement', { product: finalProducts, allProducts });
-        } 
+        const productsWithOffers = await Product.find({ 'offer': { $exists: true } });
+        res.render('offerManagement', { product: productsWithOffers, allProducts });
     } catch (error) {
         console.log(error.message);
     }
@@ -27,13 +22,11 @@ const Product = require('../models/productMdl');
         const offerPrice = req.body.offerPrice;
         const product = await Product.findOne({ productName: productName });
 
-        console.log('The product:', product);
-
         if (product.offer && product.offer.length > 0 && product.offer[0].offerPrice !== undefined) {
             console.log('Selected product already has an offer.');
             res.redirect('/admin/offerManagement?message=Selected Product already has an offer&type=error');
         } else {
-            product.offer.push({ originalPrice: product.price, offerPrice: offerPrice });
+            product.offer = offerPrice;
             await product.save();
             res.redirect('/admin/offerManagement?message=New Offer added&type=success');
         }
