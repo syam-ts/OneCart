55e634e0e4c3670723f6d8aa0b8f0bb1ce2ff067
syam. <syamnandhu3@gmail.com.>
2024-05-28 18:23:06 +0530

    coupon expiry date validation fixed

--- controllers/couponCtrl.js
+++ controllers/couponCtrl.js
@@ -32,47 +32,49 @@ const Coupon = require('../models/couponMdl');
 
 
            //<------------ adding new coupon -------------->
-    const insertCoupon = async (req, res) => {
-        try{
-            const { couponCode, description, discount , minimumAmount , expiryDate } = req.body;
-            const currentDate = new Date();
-            const existCoupon = await Coupon.findOne({couponCode : couponCode});
-           if(!couponCode){
-                res.redirect('/admin/coupon-add?message=Please enter the Coupon Code&type=error');
-            }else if(existCoupon){
-                console.log('The coupon : ',existCoupon);
-                res.redirect('/admin/coupon-add?message=Coupon Name should be unique&type=error')
-                     }else if(!description){
-                     res.redirect('/admin/coupon-add?message=Please enter the Description&type=error')
-                     }else if(description.length < 20){
-                        res.redirect('/admin/coupon-add?message=Description should have atleast 20 words&type=error');
-                    }else if(!discount){
-                        res.redirect('/admin/coupon-add?message=Please enter the Discount Amount&type=error')
-                    }else if (discount > 90 ){
-                        res.redirect('/admin/coupon-add?message=Maximum discount price should be 90%&type=error');
-                    }else if(!minimumAmount){
-                        res.redirect('/admin/coupon-add?message=Please enter the Minimum Amount&type=error');
-                    }else if(minimumAmount < 300 || minimumAmount > 40000){
-                        res.redirect('/admin/coupon-add?message=The minimum amount should be between 300 to 40000&type=error');
-                    }else if(!expiryDate){
-                        res.redirect('/admin/coupon-add?message=Please enter the Expiry Date&type=error');
-                    }else if(currentDate < expiryDate){
-                        res.redirect('/admin/coupon-add?message=Please enter valid date&type=error');
-                    }else{
-                        const coupon = new Coupon({
-                            couponCode : couponCode,
-                            description : description,
-                            discount : discount,
-                            minimumAmount : minimumAmount,
-                            expiryDate : expiryDate
-                        });
+           const insertCoupon = async (req, res) => {
+            try {
+                const { couponCode, description, discount, minimumAmount, expiryDate } = req.body;
+                const currentDate = new Date();
+                const parsedExpiryDate = new Date(expiryDate);
+                const existCoupon = await Coupon.findOne({ couponCode: couponCode });
+                if (!couponCode) {
+                    res.redirect('/admin/coupon-add?message=Please enter the Coupon Code&type=error');
+                } else if (existCoupon) {
+                    res.redirect('/admin/coupon-add?message=Coupon Name should be unique&type=error');
+                } else if (!description) {
+                    res.redirect('/admin/coupon-add?message=Please enter the Description&type=error');
+                } else if (description.length < 20) {
+                    res.redirect('/admin/coupon-add?message=Description should have at least 20 words&type=error');
+                } else if (!discount) {
+                    res.redirect('/admin/coupon-add?message=Please enter the Discount Amount&type=error');
+                } else if (discount > 90) {
+                    res.redirect('/admin/coupon-add?message=Maximum discount price should be 90%&type=error');
+                } else if (!minimumAmount) {
+                    res.redirect('/admin/coupon-add?message=Please enter the Minimum Amount&type=error');
+                } else if (minimumAmount < 300 || minimumAmount > 40000) {
+                    res.redirect('/admin/coupon-add?message=The minimum amount should be between 300 to 40000&type=error');
+                } else if (!expiryDate) {
+                    res.redirect('/admin/coupon-add?message=Please enter the Expiry Date&type=error');
+                } else if (currentDate >= parsedExpiryDate) {
+                    res.redirect('/admin/coupon-add?message=Please enter a valid expiry date&type=error');
+                } else {
+                    const coupon = new Coupon({
+                        couponCode: couponCode,
+                        description: description,
+                        discount: discount,
+                        minimumAmount: minimumAmount,
+                        expiryDate: parsedExpiryDate 
+                    });
+        
                     await coupon.save();
-                        res.redirect('/admin/coupon-list?message=New Coupon added&type=success');
-                    }
-                }catch(err){
-                    res.render('error',{ message : err.message });
+                    res.redirect('/admin/coupon-list?message=New Coupon added&type=success');
+                }
+            } catch (err) {
+                res.render('error', { message: err.message });
             }
-    };
+        };
+        
 
 
    //<------------ delete coupon -------------->
